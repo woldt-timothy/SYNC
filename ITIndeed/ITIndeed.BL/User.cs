c8c9c74a59187c21c12242d7eb1444297daa8581
Gage Vail <500152900@fvtc.edu>
2019-03-01 16:26:57 -0600

    Added BL and PL - Gage

---
+++ ITIndeed/ITIndeed.BL/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITIndeed.BL
+{
+    public class User
+    {
+        // Properties
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        // Constructors
+        public User()
+        {
+
+        }
+
+        // Methods
+
+        public void Insert()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+}
