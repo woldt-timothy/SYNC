eb6167d031dca9d566a4eb1cd74b69f0ec8f90e3
Woldt, Timothy A. (Student) <300028034@fvtc.edu>
2019-03-11 19:11:11 -0500

    Created Student and Employer Controllers with Create Views Implemented using BL

---
+++ ITIndeed/ITIndeed.MVC.UI/Controllers/StudentController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ITIndeed.BL;
+
+namespace ITIndeed.MVC.UI.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Student/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Student/Create
+        public ActionResult Create()
+        {
+            Student student = new Student();
+            return View(student);
+        }
+
+        // POST: Student/Create
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                s.StudentInsert();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(s);
+            }
+        }
+
+        // GET: Student/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Student/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
