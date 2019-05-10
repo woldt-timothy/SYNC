23b8a66cfb0279fcc46978578f86ef9778e15b26
Woldt, Timothy A. (Student) <woldt.timothy@gmail.com>
2019-05-10 12:10:20 -0500

    Fixing Merge Problem

--- ITIndeed/ITIndeed.BL/Event.cs
+++ ITIndeed/ITIndeed.BL/Event.cs
@@ -11,7 +11,7 @@ namespace ITIndeed.BL
 {
     public class Event
     {
-        // Properties
+        /// Properties
 
         public Guid Id { get; set; }
         public string Name { get; set; }
