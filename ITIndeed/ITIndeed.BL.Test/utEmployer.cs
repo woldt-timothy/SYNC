2df26f817775d1c7c4c8318e2410c38846d4c5ee
Woldt, Timothy A. (Student) <woldt.timothy@gmail.com>
2019-05-10 13:19:34 -0500

    Cleaned Up Unit Test

--- ITIndeed/ITIndeed.BL.Test/utEmployer.cs
+++ ITIndeed/ITIndeed.BL.Test/utEmployer.cs
@@ -12,36 +12,26 @@ namespace ITIndeed.BL.Test
         [TestMethod]
         public void InsertTest()
         {
-            //Tests to see if both records were inserted
-
-
             Employer employer = new Employer();
-
-
             Byte[] arrBYTE = new Byte[10000];
 
-            employer.UserName = "foxValleyBadgerMan8";
-            employer.Password = "maple";
-            employer.RepresentativeFirstName= "Bonny4438";
-            employer.RepresentativeLastName= "Clyde4438";
-            employer.Email = "bonnytheman@bemis.com38";
-            employer.Industry = "Retail38";
-            employer.OrganizationName = "Bemis Company38";
+            employer.UserName = "JoeJoe";
+            employer.Password = "pass";
+            employer.RepresentativeFirstName= "Joe";
+            employer.RepresentativeLastName= "Joe";
+            employer.Email = "jojo4597467664@gmail.com";
+            employer.Industry = "Clowning";
+            employer.OrganizationName = "Clown Store";
             employer.Phone = "888-888-8888";
             employer.ProfilePicture = arrBYTE;
             
-
             employer.EmployerInsert();
-
 
             ITIndeedEntities dc = new ITIndeedEntities();
 
             var users = dc.tblUsers;
-
             int expectedUsers = 8;
-
             int actualUsers = users.Count();
-
 
 
             var employers = dc.tblEmployers;
@@ -51,60 +41,38 @@ namespace ITIndeed.BL.Test
             int actualEmployers = employers.Count();
 
             Assert.AreEqual(expectedEmployers + expectedUsers, actualEmployers + actualUsers);
-
-
         }
 
         [TestMethod]
         public void LoadTest()
         {
-            //Tests to if Number of Employees is Equal to Number of Employees in Database
-
-
             EmployerList employers = new EmployerList();
             employers.EmployerListLoad();
 
             int expected = 6;
             int actual = employers.Count();
 
-
-
             Assert.AreEqual(expected, actual);
-
-
         }
-
-
-        
 
         [TestMethod]
         public void LoadIDTest()
         {
-            
-
-
             Employer employer = new Employer();
-            employer.EmployerLoadById(Guid.Parse("af9411dd-a4bb-4dff-8e07-24c4180caa7e"));
+            employer.EmployerLoadById(Guid.Parse("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"));
 
-
-            string expected = "Bonny4438";
+            string expected = "Joe";
             string actual = employer.RepresentativeFirstName;
 
-
-
             Assert.AreEqual(expected, actual);
-
-
         }
 
         [TestMethod]
         public void DeleteTest()
         {
-
             Employer employer = new Employer();
-            employer.EmployerId = Guid.Parse("8368a3ce-7fd2-4ac3-a786-45007c87a817");
+            employer.EmployerId = Guid.Parse("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
             employer.EmployerDelete();
-
 
             ITIndeedEntities dc = new ITIndeedEntities();
 
@@ -115,7 +83,6 @@ namespace ITIndeed.BL.Test
             int actualUsers = users.Count();
 
 
-
             var employers = dc.tblEmployers;
 
             int expectedEmployers = 5;
@@ -123,33 +90,22 @@ namespace ITIndeed.BL.Test
             int actualEmployers = employers.Count();
 
             Assert.AreEqual(expectedEmployers + expectedUsers, actualEmployers + actualUsers);
-
-
         }
-
 
         [TestMethod]
         public void UpdateTest()
         {
-
             Employer employer = new Employer();
-            employer.EmployerId = Guid.Parse("38ffe608-d859-47b6-a5b7-909301ea1c47");
-            employer.UserName = "STUFF1";
-            employer.Industry = "STUFF1";
-            //employer.EmployerUpdate();
+            employer.EmployerId = Guid.Parse("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+            employer.UserName = "NEWUSERNAME";
+            employer.Industry = "NEWINDUSTRY";
 
+            employer.EmployerLoadById(Guid.Parse("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"));
 
-            employer.EmployerLoadById(Guid.Parse("38ffe608-d859-47b6-a5b7-909301ea1c47"));
-
-            string expected = "STUFF1STUFF1";
+            string expected = "NEWUSERNAMENEWINDUSTRY";
             string actual = employer.UserName + employer.Industry;
 
             Assert.AreEqual(expected, actual);
-
-
         }
-
-
-
     }
 }
